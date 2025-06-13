60673ee98cf36935ad45aa4c6ecb4bd87d2a685a
fouad <shalabyf@my.yorku.ca>
2025-06-13 18:59:18 -0400

    Initial Commit

---
+++ Monarch/MonarchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonarchApp.swift
+//  Monarch
+//
+//  Created by Fouad on 2025-06-13.
+//
+
+import SwiftUI
+
+@main
+struct MonarchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MonarchTests/MonarchTests.swift
@@ -0,0 +1,17 @@
+//
+//  MonarchTests.swift
+//  MonarchTests
+//
+//  Created by Fouad on 2025-06-13.
+//
+
+import Testing
+@testable import Monarch
+
+struct MonarchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
